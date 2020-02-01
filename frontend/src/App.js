a6df42a4f0cac5c8d1da0128daa69d22552b1cef
joaomarcostg <joaomarcos_tg@outlook.com>
2020-01-31 22:17:26 -0300

    finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import Sidebar from '../src/components/sidebar.js'
+import Sidebar from './components/Sidebar.js'
+import Topbar from './components/Topbar.js'
+import Main from './components/Main.js'
 
 function App() {
+  const [nome, setNome] = useState('antifraude')
+
   return (
+
     <div className="App">
-      <Sidebar />
+      <Sidebar setName={setNome}/>
+      <Topbar name={nome} />
+      <Main content={nome} />
+      
     </div>
   );
 }
